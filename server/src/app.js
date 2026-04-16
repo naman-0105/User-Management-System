9910297d8a7661abea72f8dec6e2c5ac6d55c512
naman-0105 <nothingisimpossible4u2@gmail.com>
2026-04-16 20:14:21 +0530

    feat: register user routes in express app

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -8,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 export default app;
