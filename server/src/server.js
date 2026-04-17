13d5130b00d53b88824ccc8e7b624687d5bc783b
naman-0105 <nothingisimpossible4u2@gmail.com>
2026-04-17 17:27:15 +0530

    fix: configure dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
