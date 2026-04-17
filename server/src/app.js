9ce0255df69a05a4103b317fc4a12a3c7d163f4e
naman-0105 <nothingisimpossible4u2@gmail.com>
2026-04-17 14:41:06 +0530

    Update CORS configuration with allowed frontend origins

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,9 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
